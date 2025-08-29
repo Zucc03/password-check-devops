f191d92d5604ee72e11f4c2e56e0bee42697d4de
Maxim Nuss <maxim.nuss@securepoint.de>
2025-08-29 10:41:55 +0200

    added password checker unit-test

--- password_checker.py
+++ password_checker.py
@@ -12,4 +12,6 @@ def is_valid_password(password):
         return False
     if not any(c.islower() for c in password):
         return False
+    if not any(c.isupper() for c in password):
+        return False
     return True
--- test_password_checker.py
+++ test_password_checker.py
@@ -1,1 +1,23 @@
+from password_checker import is_valid_password
 
+correctPasswords = ["Password123"]
+incorrectPasswords = ["password", "password123", "Password", "PASSWORD123"]
+
+def testCorrectPasswords():
+    correctPasswordString = ", ".join(correctPasswords)
+    for password in correctPasswords:
+        assert(is_valid_password(password)) == True
+    print("% s is Correct" % correctPasswordString) 
+
+def testIncorrectPasswords():
+    incorrectPasswordString = ", ".join(incorrectPasswords)
+    for password in incorrectPasswords:
+        assert(is_valid_password(password)) == False
+    print("% s are Incorrect" % incorrectPasswordString)
+
+
+def test():
+    testCorrectPasswords()
+    testIncorrectPasswords()
+
+test()
